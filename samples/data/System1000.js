8631b20f394d6e2822dca28cc71f95d99261ef23
Artem Bohush <art.bohush@gmail.com>
2020-05-05 19:04:12 +0300

    writing transformations logic

--- samples/data/System1000.js
+++ samples/data/System1000.js
@@ -1,16 +1,50 @@
-function generateMatrix(rows, columns) {
-  const matrix = []
+function generateMatrix( rows, columns )
+{
+  const matrix = [];
 
-  for (let i = 0; i < rows; i++) {
-    const row = []
-    for (let j = 0; j < columns; j++) {
-      let value = i === j ? 1 : 0
-      row.push(value)
+  for ( let i = 0; i < rows; i++ )
+  {
+    const row = [];
+    for ( let j = 0; j < columns; j++ )
+    {
+      let value = i === j ? 1 : 0;
+      row.push( value );
     }
-    matrix.push(row)
+    matrix.push( row );
   }
 
-  return matrix
+  return matrix;
 }
 
-const matrix = generateMatrix(1000, 1000)
+function randomInteger( min, max ) 
+{
+  let rand = min - 0.5 + Math.random() * ( max - min + 1 );
+  return Math.round( rand );
+}
+
+const dimensions = 5;
+
+const matrix = generateMatrix( dimensions, dimensions );
+console.log(matrix);
+
+for ( let i = 0; i < 100000; ++i ) {
+  let row1 = randomInteger( 0, dimensions - 1 );
+  let row2;
+
+  while ( true ) 
+  {
+    row2 = randomInteger( 0, dimensions - 1 );
+
+    if ( row1 !== row2 ) 
+    break
+  }
+
+  const k = Math.random();
+
+  for ( let j = 0; j < dimensions; j++ ) 
+  {
+    matrix[row2][j] += matrix[row1][j] * k;
+  }
+}
+
+console.log(matrix);
