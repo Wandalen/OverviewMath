f2882953a4bcb7c946b5333130a7a40b0d7c9c47
Artem Bohush <art.bohush@gmail.com>
2020-05-13 12:39:47 +0300

    ndarray-linear-solver sample(good)

--- sample/ndarray-linear-solve/solve.js
+++ sample/ndarray-linear-solve/solve.js
@@ -1,33 +1,28 @@
 const solve = require('ndarray-linear-solve');
 const show = require('ndarray-show');
 const ndarray = require('ndarray');
+const _ = require( 'wTools' );
+require( 'wmathmatrix' );
+require( 'wFiles' );
+require( 'wequaler' );
 
-const matrix = require('../../data/System1000');
+var data = _.fileProvider.fileRead({
+  filePath : `${__dirname}/../../data/System1000.json`,
+  encoding : 'json',
+});
 
-const modifiedMatrix = [];
+const M = ndarray(data.M, [ 100, 100 ]);
+// console.log(M);
+const x = data.x;
+console.log(x);
+let b = ndarray(data.b);
+// console.log(b);
 
-matrix.forEach((row) =>
-{
-  'use strict';
+const xResult = ndarray(new Float64Array(100));
+const r = solve(xResult, M, b);
 
-  modifiedMatrix.push(... row);
-})
-
-const vector = [];
-
-for (let i = 0; i < 1000; i++)
-{
-  vector.push(randomInteger(-1000, 1000));
-}
-
-const A = ndarray(modifiedMatrix, [ 1000, 1000 ], [ 1, 5 ]);
-const B = ndarray(vector);
-const X = ndarray(new Float64Array(1000));
-const r = solve(X, A, B);
-// console.log('input:\n' + show(A), '\n');
-if (r)
-{
-  console.log('solution:\n' + show(X));
+if (r) {
+  console.log('solution:\n' + show(xResult));
 } else {
-  console.log('matrix is singular');
+  console.log('matrix is singular')
 }
