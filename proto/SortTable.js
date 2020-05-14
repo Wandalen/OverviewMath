a703a0e2f3ac56625aa2ea25ff8ea818ba67af0d
Artem Bohush <art.bohush@gmail.com>
2020-05-14 10:49:49 +0300

    sorting func, pause...

--- proto/SortTable.js
+++ proto/SortTable.js
@@ -1,9 +1,29 @@
-function sortTable( tableData )
+function sortTable( tableData, sortingOrder )
 {
   'use strict';
 
   const _ = require( 'wTools' );
   require( 'warraysorted' );
+
+  // const tableDataCopy = [...tableData];
+
+  // sortingOrder.forEach( ( column ) =>
+  // {
+  //   if ( column.dataTitle !== 'npmName' )
+  //   {
+  //     tableData.forEach( ( lib ) =>
+  //     {
+  //       if ( typeof lib[ column.dataTitle ] === 'boolean' )
+  //       {
+  //         return lib[ column.dataTitle ] === true
+  //       }
+  //       else if (  )
+  //       {
+          
+  //       }
+  //     } )
+  //   }
+  // } )
 
   let sortedData = [];
   for( let i = 0 ; i < tableData.length ; i++ )
@@ -12,25 +32,25 @@ function sortTable( tableData )
   sortedData.reverse();
 
   const bindingAndSolvingSLE = sortedData.filter( ( lib ) =>
-{
+  {
     if ( lib.binding && lib.solvesSLE )
     return lib;
   } );
 
   const onlyBinding = sortedData.filter( ( lib ) =>
-{
+  {
     if ( lib.binding && !lib.solvesSLE )
     return lib;
   } );
 
   const onlySolvingSLE = sortedData.filter( ( lib ) =>
-{
+  {
     if ( !lib.binding && lib.solvesSLE )
     return lib;
   } );
 
   const other = sortedData.filter( ( lib ) =>
-{
+  {
     if ( !lib.binding && !lib.solvesSLE )
     return lib;
   } );
--- proto/SortingOrder.js
+++ proto/SortingOrder.js
@@ -2,7 +2,7 @@ module.exports = [
   { readMeTitle : 'Module', dataTitle : 'npmName' },
   { readMeTitle : 'Binding', dataTitle : 'binding' },
   { readMeTitle : 'Solves SLE', dataTitle : 'solvesSLE' },
-  { readMeTitle : 'Dependents', dataTitle : 'dependents' },
   { readMeTitle : 'Node.js', dataTitle : 'supportsNodejs' },
-  { readMeTitle : 'Browser', dataTitle : 'supportsBrowser' }
+  { readMeTitle : 'Browser', dataTitle : 'supportsBrowser' },
+  { readMeTitle : 'Dependents', dataTitle : 'dependents' },
 ];
