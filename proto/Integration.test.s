1aeedbb24e899c4a9ad2741426d292fd9ecdde33
wandalen <wandalen.me@gmail.com>
2020-05-29 17:46:06 +0300

    adjust esling & integration test

--- proto/Integration.test.s
+++ proto/Integration.test.s
@@ -100,8 +100,6 @@ function samples( test )
   return ready;
 }
 
-samples.timeOut = 60000;
-
 //
 
 function eslint( test )
@@ -123,7 +121,7 @@ function eslint( test )
     throwingExitCode : 0
   } )
 
-  //
+  /* */
 
   ready.then( () =>
   {
@@ -136,7 +134,7 @@ function eslint( test )
     return null;
   } )
 
-  //
+  /* */
 
   if( fileProvider.fileExists( sampleDir ) )
   ready.then( () =>
@@ -153,8 +151,6 @@ function eslint( test )
   return ready;
 }
 
-eslint.timeOut = 120000;
-
 // --
 // declare
 // --
@@ -162,14 +158,15 @@ eslint.timeOut = 120000;
 var Self =
 {
 
-  name : 'Tools.Math.Integration',
+  name : 'Overview.Math.Integration',
   silencing : 1,
   enabled : 1,
+  routineTimeOut : 300000,
 
   tests :
   {
     samples,
-    eslint
+    eslint,
   },
 
 }
