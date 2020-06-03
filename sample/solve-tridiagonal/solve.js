241f39662a63ae3dd496ce2b9008312ac14ab30d
ArtemBohush <art.bohush@gmail.com>
2020-06-03 15:47:16 +0300

    not working lib(solve-tridiagonal) sample deleted

--- sample/solve-tridiagonal/solve.js
+++
@@ -1,8 +0,0 @@
-var tdiag = require( 'solve-tridiagonal' )
-
-var d = [ 4, 25, -5 ]
-
-tdiag( [ 0, -1, 2 ], [ 2, 7, -3 ], [ 1, 4, 0 ], d )
-console.log( d );
-// => d = [ 1, 2, 3 ]
-// помилка при виконанні прикладу з readme
