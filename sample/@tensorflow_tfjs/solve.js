90d4e5abf681d08adaea22314e6cc21ecc335714
ArtemBohush <art.bohush@gmail.com>
2020-06-03 14:46:51 +0300

    ndarray-linear-solver sample

--- sample/@tensorflow_tfjs/solve.js
+++ sample/@tensorflow_tfjs/solve.js
@@ -1,21 +1,21 @@
-const tf = require( '@tensorflow/tfjs-node' );
-const _ = require( 'wTools' );
-require( 'wmathmatrix' );
-require( 'wFiles' );
-require( 'wequaler' );
+// const tf = require( '@tensorflow/tfjs-node' );
+// const _ = require( 'wTools' );
+// require( 'wmathmatrix' );
+// require( 'wFiles' );
+// require( 'wequaler' );
 
-const createMatrix = require( '../../proto/CreateMatrix' );
+// const createMatrix = require( '../../proto/CreateMatrix' );
 
-var data = _.fileProvider.fileRead( {
-  filePath : `${__dirname}/../../data/System1000.json`,
-  encoding : 'json',
-} );
+// var data = _.fileProvider.fileRead( {
+//   filePath : `${__dirname}/../../data/System1000.json`,
+//   encoding : 'json',
+// } );
 
-const M = createMatrix( data.M );
-// console.log(M);
-const x = data.x;
-console.log( x );
-let b = data.b;
-// console.log(b);
+// const M = createMatrix( data.M );
+// // console.log(M);
+// const x = data.x;
+// console.log( x );
+// let b = data.b;
+// // console.log(b);
 
-tf.linalg.solve( M, b, false );
+// tf.linalg.solve( M, b, false );
